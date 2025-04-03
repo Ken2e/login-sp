ccbf166bb939b3f9ed3acdd11fb4c32264c687db
Ken2e <kendrickpromise@gmail.com>
2025-04-03 12:37:18 +0100

    added java 22

---
+++ src/main/java/org/example/zombie/config/WebConfig.java
@@ -0,0 +1,25 @@
+package org.example.zombie.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+//CORS - Cross Origin Configuration
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")  //api/users/list
+                        .allowedOrigins("*")
+                        .allowedMethods("Get", "Post", "Put", "Delete")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
